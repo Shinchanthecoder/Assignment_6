8748f3bdbee9cc72390e09008641d35c81bc8ae9
Shinchanthecoder <123757382+Shinchanthecoder@users.noreply.github.com>
2023-01-27 23:13:13 +0530

    Create Q6.py

---
+++ Q6.py
@@ -0,0 +1,8 @@
+def studentInfo(student_id, student_name = None, student_class = None):
+    print(student_id)
+    print(student_name)
+    print(student_class)
+
+studentInfo(22105102)
+print("\n")
+studentInfo(22105102, 'Mayank sajnani', 'ECE')
